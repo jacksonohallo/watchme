12a46eca30c4c61e1abfc28f606413af565c6ead
jacksonohallo <jacksonohallo@gmail.com>
2022-09-19 17:27:41 +0300

    fetch data from api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import requests from './requests';
 import './App.css';
+import Row from './Row';
 
 
 function App() {
   return (
     <div className="App">
+      
       <h1>Netflix clone</h1>
+
+      
+     < Row title="NETFLIX ORIGINAL" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Trending" fetchUrl={requests.fetchTrending} />
     </div>
   );
 }
