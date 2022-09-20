ac9f833996a0ac928745dca0c55a2b4c8c88be71
jacksonohallo <jacksonohallo@gmail.com>
2022-09-20 13:31:09 +0300

    add api keys and endpoints

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-
+import Home from './components/pages/Home'
+import {Routes,Route} from 'react-router-dom'
 function App() {
   return (
-    <div className="App">
-      
+    <> 
       <Navbar/>
-
-    </div>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+      </Routes>
+      
+    </>
   );
 }
 
---
+++ src/components/Main.jsx
@@ -0,0 +1,18 @@
+
+import React, { useState } from 'react'
+
+const Main = () => {
+
+const [movies,SetMovies] = useState([])
+
+    
+    
+    
+    
+
+  return (
+    <div>Main</div>
+  )
+}
+
+export default Main
