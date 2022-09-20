10e865ea7eafb7c35e0b692474ccc5a6756dff75
jacksonohallo <jacksonohallo@gmail.com>
2022-09-20 18:08:59 +0300

    add rows to display movies in different categories

---
+++ src/components/Rows.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Rows = () => {
+  return (
+    <div>
+    
+        
+    </div>
+  )
+}
+
+export default Rows
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Main from '../Main'
+import requests from '../Requests'
+import Rows from '../Rows'
 
 const Home = () => {
   return (
     <div>
-      <Main/>
+      <Main />
+      <Rows title='Up Coming' fetchURL={requests.requestUpcoming} />
+      <Rows title='Popular' fetchURL={requests.requestPopular} />
+      <Rows title='Trending' fetchURL={requests.requestTrending} />
+      <Rows title='Top Rated' fetchURL={requests.requestTopRated} />
+      <Rows title='Horrow' fetchURL={requests.requestHorror} />
     </div>
   )
 
